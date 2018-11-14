d85e1e118e956158a64213087526a9fb799384db
Chriz <chriz@Chrizs-iMac.local>
2018-11-14 17:24:36 +0800

    Initial commit

---
+++ ext11.py
@@ -0,0 +1,8 @@
+print "how old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How muchb do you weigh?",
+weight = raw_input()
+
+print "So,you're %s old, %s tall and %s heavy." % (age,height,wei20ght)
